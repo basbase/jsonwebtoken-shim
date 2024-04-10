db5bb68b3dbbe1218e9dbc875506946f9939a923
basbase <github@basbase.com>
2024-04-10 22:56:25 +0200

    add decode

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 import {KJUR} from 'jsrsasign';
 
 exports.Algorithm= {};
-exports.decode= function() {};
-exports.sign= function() {};
+exports.decode= function(value) {
+    console.debug('SEBUG decode');
+    return value ? KJUR.jws.JWS.parse(value) : {};
+};
+exports.sign= function() {
+    console.debug('SEBUG sign');
+};
 exports.verify= function(value, key) {
+    console.debug('SEBUG verify');
     return KJUR.jws.JWS.verify(value, key)
 };
 
