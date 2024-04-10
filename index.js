399c61ba4c2d9975d85e5d1bbc4f6efd22b04ee7
basbase <github@basbase.com>
2024-04-10 22:34:02 +0200

    add verify

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //import { Algorithm, decode, sign, verify } from 'jsonwebtoken';
+import {KJUR} from 'jsrsasign';
 
 exports.Algorithm= {};
 exports.decode= function() {};
 exports.sign= function() {};
-exports.verify= function() {};
+exports.verify= function(value, key) {
+    return KJUR.jws.JWS.verify(value, key)
+};
 
 module.exports = exports;
