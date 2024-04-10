ee6412ddff3b80cfc18caf7db914732f62fbec7d
basbase <github@basbase.com>
2024-04-10 21:54:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//import { Algorithm, decode, sign, verify } from 'jsonwebtoken';
+
+exports.Algorithm= {};
+exports.decode= function() {};
+exports.sign= function() {};
+exports.verify= function() {};
+
+module.exports = exports;
